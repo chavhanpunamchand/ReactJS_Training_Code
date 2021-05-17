a3793fc80bc240b2775993f4409d774e7436b916
deq <chavhanpunamchand@gmail.com>
2021-05-17 15:37:38 +0530

    React_Redux example

---
+++ reduxapp/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css';
+import {useSelector,useDispatch} from "react-redux"
+import {incNumber,decNumber} from "./actions/index"
+
+const App = () =>{
+  const myState= useSelector((state)=> state.changeTheNumber);
+  const dispatch=useDispatch();
+  return(
+    <>
+     <div class="container">
+  
+  <h1>Increment/Decrement counter</h1>
+  <h4>using React and Redux</h4>
+  
+  <div class="quantity">
+    < a class="quantity__minus" title="Decrement"onClick={ () => dispatch(decNumber(5)) }> <span>-</span></a>
+    <input name="quantity" type="text" class="quantity__input" value={myState} />
+    <a class="quantity__plus" title="Increment" onClick={ () => dispatch(incNumber(5))}> <span>+</span></a>
+  </div>
+
+      </div>
+
+    </>
+  )
+} 
+
+
+export default App;
